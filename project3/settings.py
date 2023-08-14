399994230ee794f16a550b7258993e2e9a06f46a
John Mark Delima <delimajohnmark14@gmail.com>
2023-08-14 21:50:32 +0800

    Update README.md

--- project3/settings.py
+++ project3/settings.py
@@ -129,5 +129,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles_build'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
