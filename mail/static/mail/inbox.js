0e287efd2f3893cb1cc4ee27fabbf21cc8c63ea2
John Mark Delima <102164012+jnale-hub@users.noreply.github.com>
2023-12-07 13:58:51 +0800

    Update inbox.js

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -106,7 +106,7 @@ function view_email(id) {
 }
 
 function load_mailbox(mailbox) {
-
+  
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -114,8 +114,8 @@ function load_mailbox(mailbox) {
   document.querySelector('#body-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML += `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
   const loading = document.createElement('div');
   loading.innerHTML = '<p>Fetching your emails...</p>';
 
@@ -131,6 +131,7 @@ function load_mailbox(mailbox) {
         element.innerHTML = 'No available mail.';
         document.querySelector('#emails-view').appendChild(element);
       } else {
+
         // Loop emails and create a div 
         emails.forEach(email => {
           const element = document.createElement('div');
@@ -144,10 +145,10 @@ function load_mailbox(mailbox) {
           const day = timestampDate.getDate();
 
           element.innerHTML = `
-            <div id="sender"><b>${email.sender}</b></div>
-            <div id="subject">${email.subject}</div>
-            <div id="timestamp" class="d-lg-none">${month} ${day}</div>
-            <div id="timestamp" class="d-none d-lg-block">${email.timestamp}</div>
+          <div id="sender"><b>${email.sender}</b></div>
+          <div id="subject">${email.subject}</div>
+          <div id="timestamp" class="d-lg-none">${month} ${day}</div>
+          <div id="timestamp" class="d-none d-lg-block">${email.timestamp}</div>
           `;
 
           element.addEventListener('click', () => view_email(email.id));
@@ -155,11 +156,7 @@ function load_mailbox(mailbox) {
           document.querySelector('#emails-view').append(element);
         });
       }
-    })
-    .catch(error => {
-      // Handle errors, e.g., display an error message
-      document.querySelector('#emails-view').innerHTML = `<p>Error: ${error.message}</p>`;
-    });
+  });
 }
 
 
